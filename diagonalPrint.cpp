9891e32710f372ecf2a169efef9d21442220c21f
AniketGhodake <ghodakeaniket27@gmail.com>
2024-02-15 13:54:52 +0530

    15th Feb 2024

---
+++ diagonalPrint.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+void diagonalOfArray(int arr[][3], int row, int col){
+    cout<<"The Diagonal elements of the given array is as follows :  ";
+    for(int i = 0; i < row; i++){
+        cout << arr[i][i]<<" ";
+    }
+}
+
+int main(){
+    int arr[3][3];
+    int row = 3; 
+    int col = 3;
+
+    cout<<"Enter the "<<row * col <<" array elements : "<<endl;
+    for(int i = 0; i<row; i++){
+        for(int j = 0; j<col; j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    cout<<"The array elements that you have entered is as follows : "<<endl;
+    for(int i = 0; i < row; i++){
+        for(int j = 0; j < col; j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    diagonalOfArray(arr,row,col);
+}
