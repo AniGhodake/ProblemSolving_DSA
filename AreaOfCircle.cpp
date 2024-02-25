0c2aa2fbddd73ca889fdea71f89b832b1ba7bc6e
AniketGhodake <ghodakeaniket27@gmail.com>
2024-02-25 21:28:56 +0530

    25th Feb 2024

---
+++ AreaOfCircle.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+#define Pi 3.14159
+
+float calculateArea(float radius){
+    float area = Pi*radius*radius;
+    return area;
+
+}
+int main(){
+    // float radius;
+    // cout<<"Enter the Value of Radius"<<endl;
+    // cin>>radius;
+    // float area =  calculateArea(radius);
+    // cout<<"Area of circle is : "<<area;
+
+}
