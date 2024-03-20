c8cd2d067e96f7c25bd3e729b6d90282aa75af53
AniketGhodake <ghodakeaniket27@gmail.com>
2024-03-20 11:13:25 +0530

    20th  March 2024

---
+++ FancyPattern1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"Enter the Value of Num"<<endl;
+    cin>>num;
+    int count=1;
+    int row;
+    for(row=0;row<num/2;row++){                    //  pending examlple
+
+     
+}
+}
