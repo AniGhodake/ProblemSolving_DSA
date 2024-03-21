671d000cb9b4e914fd5e50b99c68f7ffc681a187
AniketGhodake <ghodakeaniket27@gmail.com>
2024-03-21 10:18:59 +0530

    21st  March 2024

---
+++ FancyPattern3.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"enter the num"<<endl;
+    cin>>num;
+    int count;
+    // for(int row=0;row<num;row++){
+    //     count=1;
+    //     for(int col=0;col<2*row+1;col++){
+    //         if(col==0 ||col==2*row){
+    //             cout<<"* ";
+    //         }
+    //         else{
+                
+    //             for(;col<row+1;){
+    //                 cout<<count++<<" ";
+    //             }
+    //             for(;col<2*row;){
+    //                 cout<<"$ ";
+    //             }
+               
+    //         }
+    //     }
+    //     cout<<endl;
+    // }
+}
