b7130abcb3c4b7ca265740d10aacdd17e19c587d
AniketGhodake <ghodakeaniket27@gmail.com>
2024-03-22 11:19:03 +0530

    22nd March 2024

---
+++ Butterfly.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"Enter the num";
+    cin>>num;
+    int half=num/2;
+    // for(int row=0;row<num;row++){
+    //     for(int col=0;col<num;col++){
+    //        if(col<row+1)
+    //             cout<<"* ";
+    //         if(col>num-2){
+    //             cout<<"  ";
+    //         }
+    //         else{
+    //             cout<<"* ";
+    //         }
+            
+            
+    //     }
+    //     cout<<endl;
+    // }
+}
