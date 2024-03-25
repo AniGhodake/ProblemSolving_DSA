95e0e83868e45bb252222ff6217aafaaafda4a9a
AniketGhodake <ghodakeaniket27@gmail.com>
2024-03-25 09:51:40 +0530

    25th March 2024

---
+++ EvenOdd1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+bool isEven(int num){
+    if(num%2==0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+int main(){
+    int num;
+    cout<<"Enter the Number"<<endl;
+    cin>>num;
+    bool result = isEven(num);
+    if(result){
+        cout<<"Entered number "<<num<<" is Even Number";
+    }
+    else{
+        cout<<"Entered number "<<num<<" is Odd Number";
+    }
+}
