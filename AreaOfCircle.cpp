2bc5160811ee695e3a2a3855278a049ce54802f0
Aniket Sunil Ghodake <74956511+AniGhodake@users.noreply.github.com>
2024-03-31 23:03:08 +0530

    Update AreaOfCircle.cpp

--- AreaOfCircle.cpp
+++ AreaOfCircle.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 #define Pi 3.14159
 
-// float calculateArea(float radius){
-//     float area = Pi*radius*radius;
-//     return area;
+float calculateArea(float radius){
+    float area = Pi*radius*radius;
+    return area;
 
-// }
+}
 int main(){
-    // float radius;
-    // cout<<"Enter the Value of Radius"<<endl;
-    // cin>>radius;
-    // float area =  calculateArea(radius);
-    // cout<<"Area of circle is : "<<area;
+    float radius;
+    cout<<"Enter the Value of Radius"<<endl;
+    cin>>radius;
+    float area =  calculateArea(radius);
+    cout<<"Area of circle is : "<<area;
 
 }
