3cf2617c610c2152ed590aa9b62435b6ee17925b
Aniket Sunil Ghodake <74956511+AniGhodake@users.noreply.github.com>
2024-08-09 23:56:21 +0530

    Update DigitsOfInteger.cpp

--- DigitsOfInteger.cpp
+++ DigitsOfInteger.cpp
@@ -1,14 +1,14 @@
 #include<iostream>
 using namespace std;
-void DigitSeparation(int num){
-    int digit;
-    cout<<"Digits Of the Integer "<<num<<" is as follows :"<<endl;
-    while(num){
-        digit = num%10;
-        cout<<digit<<" "<<endl;
-        num = num/10;
-    }
-}
+// void DigitSeparation(int num){
+//     int digit;
+//     cout<<"Digits Of the Integer "<<num<<" is as follows :"<<endl;
+//     while(num){
+//         digit = num%10;
+//         cout<<digit<<" "<<endl;
+//         num = num/10;
+//     }
+// }
 int main(){
     int num;
     cout<<"Enter the Number to find its Digits : "<<endl;
