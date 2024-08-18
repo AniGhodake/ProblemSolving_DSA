17e36ee3598912830e6dc88ebb264a38b539362d
Aniket Ghodake <ghodakeaniket27@gmail.com>
2024-08-18 20:50:00 +0530

    18th August 2024

---
+++ SortColors.cpp
@@ -0,0 +1,25 @@
+//        ------------------Leetcode Problem 75 ---------------------
+
+
+
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int left = 0, i = 0, right = nums.size()-1;
+        while(i<=right){
+            // if(nums[i]==0){
+            //     swap(nums[i],nums[left]);
+            //     left++;
+            //     i++;
+            // }
+            // else if(nums[i]==2){
+            //     swap(nums[i],nums[right]);
+            //     right--; ww
+            // }
+            // else{
+            //     i++;
+            // }
+        }
+    }
+};
