e8b9ccf34dd078e78495c71f4efb1f789c90c9b5
Aniket Ghodake <ghodakeaniket27@gmail.com>
2024-09-16 12:52:39 +0530

    16th September 2024

---
+++ FirstwebappApplication.java
@@ -0,0 +1,16 @@
+package com.tutorialspringboot.firstwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstwebappApplication.class, args);
+	}
+
+}
+// /META-INF/resources/webjars/bootstrap/5.1.3/css/bootstrap.min.css
+// /META-INF/resources/webjars/bootstrap/5.1.3/js/bootstrap.min.js
+// /META-INF/resources/webjars/jquery/3.6.0/jquery.min.js
