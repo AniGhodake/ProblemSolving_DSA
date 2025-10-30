44d934fd8056168832a831ebc221ee9d1e9e2703
Aniket Ghodake <ghodakeaniket27@gmail.com>
2025-10-30 19:59:45 +0530

    30th October 2025

---
+++ DiceRoll.java
@@ -0,0 +1,11 @@
+class DiceRoll{
+    public static void main(String args[]){
+        System.out.println("\nWelcome to Dice roll : ");
+
+        // for(int i = 0; i < 50; i++){
+        //     int num = (int)Math.ceil(Math.random() * 6);
+        //     System.out.print(num+" ");
+        // }
+        int num = (int)Math.ceil(Math.random() * 6);
+    }
+}
