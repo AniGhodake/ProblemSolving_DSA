a710544065b781ebb91baad8b3596d0a6421319c
Aniket Ghodake <ghodakeaniket27@gmail.com>
2025-10-31 19:57:36 +0530

    31st October 2025

---
+++ EvenOddBitwise.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class EvenOddBitwise{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Number : ");
+        int num = sc.nextInt();
+
+        if((num & 1) == 0){
+            System.out.println("Even Number");
+        }else{
+            System.out.println("Odd Number");
+        }
+    }
+}
